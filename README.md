887f1412ef3c56a67233835821b243f5f96938d7
Yoon, Seongsu <sople1@snooey.net>
2015-02-24 14:03:36 +0900

    init commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# Laravel5 lang for Korean
+Laravel5 한국어 언어 파일
+5 버전부터 달라진 규칙 및 추가된 문장 반영
+
+다음 저장소 데이터를 사용하였습니다.
+[https://github.com/thisiskaden/laravel4-lang-korean]
---
+++ passwords.php
@@ -0,0 +1,22 @@
+<?php
+
+return [
+
+	/*
+	|--------------------------------------------------------------------------
+	| Password Reminder Language Lines
+	|--------------------------------------------------------------------------
+	|
+	| The following language lines are the default lines which match reasons
+	| that are given by the password broker for a password update attempt
+	| has failed, such as for an invalid token or invalid new password.
+	|
+	*/
+
+	"password" => "비밀번호는 최소한 6자리 이상이며 일치해야 합니다.",
+	"user" => "이 이메일을 사용하는 사용자를 찾을 수 없습니다.",
+	"token" => "이 비밀번호 초기화 토큰은 유효하지 않습니다.",
+	"sent" => "비밀번호 초기화 링크를 이메일로 보내드렸습니다.",
+	"reset" => "비밀번호를 초기화하였습니다.",
+
+];
